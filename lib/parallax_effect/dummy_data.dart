233bd731e6723872e16364631d81f7491b2ced02
Armağan Gök <1armagangok@gmail.com>
2022-08-08 20:55:25 +0300

    parallax animation

---
+++ lib/parallax_effect/dummy_data.dart
@@ -0,0 +1,30 @@
+List<Map> paintings = [
+  {
+    'image': 'assets/painting6.jpeg',
+    'name': 'The Sower',
+  },
+  {
+    'image': 'assets/painting2.jpeg',
+    'name': 'The Starry Night',
+  },
+  {
+    'image': 'assets/painting3.jpeg',
+    'name': 'The Potato Eaters',
+  },
+  {
+    'image': 'assets/painting4.jpeg',
+    'name': 'Irises',
+  },
+  {
+    'image': 'assets/painting5.jpeg',
+    'name': 'Starry Night Over the Rhône',
+  },
+  {
+    'image': 'assets/painting7.jpeg',
+    'name': 'Landscape with Snow',
+  },
+  // {
+  //   'image': 'assets/painting8.jpeg',
+  //   'name': 'The Night Café',
+  // },
+]; 
---
+++ lib/parallax_effect/parallax_view.dart
@@ -0,0 +1,154 @@
+import 'package:flutter/material.dart';
+
+import './dummy_data.dart';
+
+class ParallaxView extends StatefulWidget {
+  const ParallaxView({Key? key}) : super(key: key);
+
+  @override
+  ParallaxViewState createState() => ParallaxViewState();
+}
+
+class ParallaxViewState extends State<ParallaxView> {
+  late PageController pageController;
+  double pageOffset = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    pageController = PageController(viewportFraction: 0.7);
+    pageController.addListener(() {
+      setState(() {
+        pageOffset = pageController.page!;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        decoration: decoration(),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: vanGoghInformationTexts(),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 20, bottom: 20),
+                  child: pageViewCaption(),
+                ),
+                Container(
+                  height: 400,
+                  padding: const EdgeInsets.only(bottom: 30),
+                  child: buildPageView(),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  BoxDecoration decoration() {
+    return const BoxDecoration(
+      image: DecorationImage(
+        image: AssetImage("assets/bg.jpeg"),
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+
+  Column vanGoghInformationTexts() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: const [
+        SizedBox(height: 30),
+        Text(
+          'Vincent\nvan Gogh',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 50,
+            letterSpacing: 2,
+          ),
+        ),
+        SizedBox(height: 10),
+        Text(
+          '30 March 1853-29 July 1890',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontStyle: FontStyle.italic,
+          ),
+        ),
+        SizedBox(height: 10),
+        Text(
+          'Vincent Willem van Gogh was a Dutch post-impressionist painter who posthumously became one of the most famous and influential figures in the history of Western art.',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 15,
+            fontStyle: FontStyle.italic,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Text pageViewCaption() {
+    return const Text(
+      'Highlight Paintings',
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: 20,
+      ),
+    );
+  }
+
+  Widget buildPageView() {
+    return PageView.builder(
+      itemCount: paintings.length,
+      controller: pageController,
+      itemBuilder: (context, i) {
+        return Transform.scale(
+          scale: 1,
+          child: Container(
+            padding: const EdgeInsets.only(right: 20),
+            child: Stack(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(15),
+                  child: Image.asset(
+                    paintings[i]['image'],
+                    height: 370,
+                    fit: BoxFit.cover,
+                    alignment: Alignment(-pageOffset.abs() + i, 0),
+                  ),
+                ),
+                Positioned(
+                  left: 10,
+                  bottom: 20,
+                  right: 10,
+                  child: Text(
+                    paintings[i]['name'],
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 35,
+                      fontStyle: FontStyle.italic,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
